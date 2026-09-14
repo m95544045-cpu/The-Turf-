0a24b161f5f9458a0a0d6e7a1761dbb7f6ade9ee
remssuthar <remssuthar1213@gmail.com>
2026-09-14 21:46:07 +0530

    Configure GitHub Pages project path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Turf/',
+  base: '/Turfview-/',
   appType: 'spa',
   server: {
     host: '0.0.0.0',
